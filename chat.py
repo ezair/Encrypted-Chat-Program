02ffdb8ffb2829bd7ae00f7e34ee20a43211b426
EZ <zaireric@gmail.com>
2018-04-25 16:50:14 -0400

    just more updates

--- chat.py
+++ chat.py
@@ -15,6 +15,91 @@ Description:	This program runs a peer to peer
 					aes.py
 '''
 import socket
-import time
+import sys
+import threading
+import getpass
 from elgamal import generatePrivateKey
 from elgamal import generateGenerator
+
+
+#Server thread method
+def serverThread(s):
+    global session_open
+    global username
+
+    print("Enter '/q' to quit chat")
+    while True:
+        if session_open:
+            try:
+                str = s.recv(100).decode()
+                print("\r>>>" + username + str + "\n<<<", end="", flush=True)
+            except:
+                print("Connection closed")
+                break
+            if str == "/q":
+                session_open = 0;
+                break
+        print("Connection closed")
+        s.close()
+        sys.exit()
+
+
+#Client thread method.
+def clientThread(s):
+    global session_open
+    while True:
+        if session_open:
+            str = input("<<< " )
+            s.send(str.encode())
+            if str == "/q":
+                print("Chat termination signal sent!!!")
+                session_open = 0;
+                s.close()
+                sys.exit()
+
+
+#required globals.
+option = int(input("\n(1)Start a session\n(2)Connect to a session\nSelect an option: "))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+session_open = 0
+
+
+#user creates a session. Must wait for another user to join.
+if option == 1:
+	host = input("Enter your session IP address: ")
+	port = int(input("Enter your session port number: "))
+	password = getpass.getpass("Enter your session password: ")
+	username = input("Enter your session username: ")
+
+	#create the session using given info.
+	s.bind((host, port))
+	s.listen(2)
+	print("Waiting for a client to connect...")
+	
+	#open the server and wait for things to happen.
+	while True:
+		c, addr = s.accept()
+		threading.Thread(target=serverThread, args=(s,)).start()
+		threading.Thread(target=clientThread, args=(s,)).start()
+	
+
+#user is connecting to a session. Must enter correct session info.
+elif option == 2:
+	host = input("Enter session IP: ")
+	port = int(input("Enter session Port: "))
+	password = getpass.getpass("Enter session password: ")
+
+	#attempt to connect to the session
+	try:
+		s.connect((host, port))
+		threading.Thread(target=serverThread, args=(c,)).start()
+		threading.Thread(target=clientThread, args=(c,)).start()
+	except socket.error:
+		print("Information is incorrect")
+		sys.exit()
+
+
+#user enterd a non existing option
+else:
+	print("Incorrect option")
+	sys.exit()
