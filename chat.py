e3d7cc78cded6b84fb3e09ad2699d174193646e6
EZ <zaireric@gmail.com>
2018-04-27 11:39:33 -0400

    Changed the name of a few files and made some cleanups

--- chat.py
+++ chat.py
@@ -19,7 +19,7 @@ import socket
 from threading import Thread
 from getpass import getpass
 from os import system
-from elgamal import generatePrivateKey, generateGenerator
+from Elgamal import generatePrivateKey, generateGenerator
 
 
 #Thread used for receiving messages.
@@ -79,48 +79,71 @@ def sendMsg(s):
             sys.exit()
 
 
-#required global vairables.
-option = int(input("\n(1)Start a session\n(2)Connect to a session\nSelect an option: "))
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-session_open = 1
-
-#_______________________________________________________________________________________
-
-#user creates a session. 
-#Must wait for another user to join.
-if option == 1:
+#Create a session (as a server) and wait for a client to connect.
+#Once a client successfully connects, computers begin to chat.
+#paramaters:
+#			socket s (used to host connection)
+#return type: void
+def startSession(s):    
+    global username
     host = input("Enter your session IP address: ")
     port = int(input("Enter your session port number: "))
     password = getpass("Enter your session password: ")
     username =  "<" + input("Enter your session username: ") + "> "
-
     #create the session using given info.
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.listen(5)
     print("Waiting for a client to connect...")
-
     #start therads
     c, addr = s.accept()
     Thread(target=receiveMsg, args=(c,)).start()
     Thread(target=sendMsg, args=(c,)).start()
 
 
-#user is connecting to a session.
-#Must enter correct session info.
-elif option == 2:
+#Join a session (as a client)
+#Once successfully connected, computers begin to chat
+#paramaters:
+#			socket s (used to join connection)
+#return type: void
+def connectToSession(s):    
+    global username
     host = input("Enter session IP: ")
     port = int(input("Enter session Port: "))
     password = getpass("Enter session password: ")
     username = "<" + input("Enter your session name: ") + "> "
-
-#attempt to connect to the session
+    #attempt to connect to the session
     s.connect((host, port))
+    #start threads
     Thread(target=receiveMsg, args=(s,)).start()
     Thread(target=sendMsg, args=(s,)).start()
 
 
-#user entered a non-existent option
-else:
-    print("Incorrect option")
-    sys.exit()
+#______________________________________________________________________________________
+#required global vairables.
+session_open = 1
+username = ""
+#_______________________________________________________________________________________
+
+
+def main():
+	option = int(input("\n(1)Start a session\n(2)Connect to a session\nSelect an option: "))
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	#user creates a session. 
+	#Must wait for another user to join.
+	if option == 1:
+	    startSession(s)
+
+	#user is connecting to a session.
+	#Must enter correct session info.
+	elif option == 2:
+	    connectToSession(s)
+
+	#user entered a non-existent option
+	else:
+	    print("Incorrect option")
+	    sys.exit()
+
+if __name__ == '__main__':
+	main()
